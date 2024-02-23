c350ecf3abea2faa5ba77085dcb4d418903175a8
Wellington Silverio <wellington.alves.silverio@gmail.com>
2024-02-23 02:48:23 -0300

    adjust prompt

--- describer.js
+++ describer.js
@@ -2,6 +2,11 @@ const exec = require('child_process').exec;
 const OpenAI = require('openai');
 const package = require('./package.json');
 
+if (!package?.describer?.openai_token) {
+    console.log("Configure you token in package.json");
+    return;
+}
+
 const openai = new OpenAI({
     apiKey: package?.describer?.openai_token,
 });
@@ -20,17 +25,22 @@ function execProcess(command, cb) {
 
 execProcess("git --no-pager log HEAD...main --format='%ai;%an;%B'", async function (err, response) {
     if (!err) {
-        const chatCompletion = await openai.chat.completions.create({
-            messages: [
-                { role: 'system', content: "I have some git commit titles from my work with the node program that I'm doing and I would like to create a description for the github pull request explaining what I did to the reviewer, please don't use commit names" },
-                { role: 'system', content: "The title is divided into: datetime;author name;title of changes" },
-                { role: 'user', content: response },
-            ],
-            model: 'gpt-3.5-turbo',
-            temperature: 0.1,
-        });
-        console.log(chatCompletion?.choices[0]?.message?.content);
-
+        try {
+            const chatCompletion = await openai.chat.completions.create({
+                messages: [
+                    { role: 'system', content: "I have some git commit titles from my work with the node program that I'm doing and I would like to create a description for the github pull request explaining what I did to the reviewer, please don't use commit names" },
+                    { role: 'system', content: "Please, add Changes Made, Details and message thanking the reviewer" },
+                    { role: 'system', content: "The title is divided into: datetime;author name;title of changes" },
+                    { role: 'user', content: response },
+                ],
+                model: 'gpt-3.5-turbo',
+                temperature: 0.01,
+            });
+    
+            console.log(chatCompletion?.choices[0]?.message?.content);
+        } catch (error) {
+            console.log(error);
+        }
     } else {
         console.log(err);
     }
